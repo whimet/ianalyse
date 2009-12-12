4630d57633174d05a9ce348975bbf36b50bcf87f
hukai <hukai@localhost>
2009-12-12 15:04:04 +0800

    reduced the cclive logs to five to make the hg push easier.

--- analyse/tests/osutils_tests.py
+++ analyse/tests/osutils_tests.py
@@ -10,12 +10,12 @@ class OSUtilsTests(TestCase):
         
     def testShouldSortTheFilesBasedOnTheRuleAsc(self):
         files = os.sort_by_rule(self.root, 'log([0-9]*).*.xml', 'asc')
-        self.assertEquals(55, len(files))
-        self.assertEquals('log20080924062941.xml', files[54])
-        self.assertEquals('log20080624064201Lbuild.70.xml', files[0])
+        self.assertEquals(5, len(files))
+        self.assertEquals('log20080924062941.xml', files[4])
+        self.assertEquals('log20080923021338.xml', files[0])
 
     def testShouldSortTheFilesBasedOnTheRuleDesc(self):
        files = os.sort_by_rule(self.root, 'log([0-9]*).*.xml', 'desc')
        self.assertEquals('log20080924062941.xml', files[0])
-       self.assertEquals('log20080624064201Lbuild.70.xml', files[54])     
+       self.assertEquals('log20080923021338.xml', files[4])     
                                                                      
