708db1ecc60aec42328a585cee371a5544238c44
hukai <hukai@localhost>
2009-12-19 15:16:21 +0800

    removed the method total on Build since it is not used any more.

--- analyse/tests/__init__.py
+++ analyse/tests/__init__.py
@@ -2,7 +2,6 @@ import unittest
 import doctest
 import analyse.tests.build_tests
 import analyse.tests.build_factory_tests
-import analyse.tests.build_total_pass_percentage_tests
 import analyse.tests.successful_rate_chart
 import analyse.tests.datetimeutils_tests
 import analyse.tests.osutils_tests
@@ -22,7 +21,6 @@ def suite():
     else :
         s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.build_tests.BuildTest))
         s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.build_factory_tests.BuildFactoryTest))
-        s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.build_total_pass_percentage_tests.BuildTotalPassPercentageTest))
         s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.successful_rate_chart.SuccessfulRateChartTests))
         s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.datetimeutils_tests.DatetimeUtilsTest))
         s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.builds_tests.BuildsTest))
