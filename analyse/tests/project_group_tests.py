f61e01949982b96444f8d79e5f70615c5b1ae6d7
hukai <hukai@localhost>
2009-12-19 15:38:09 +0800

    removed the database dependency.

---
+++ analyse/tests/project_group_tests.py
@@ -0,0 +1,7 @@
+# def test_should_not_remove_records_of_other_project(self):
+#     self.assertEqual(0, len(Build.objects.all()))
+#     builds = Builds.create_builds(TestUtils().connectfour_config(), BuildFactoryTest.PATTERN);
+#     self.assertEqual(2, len(Build.objects.all()))
+#     builds = Builds.create_builds(TestUtils().cclive_config(), None);        
+#     self.assertEqual(7, len(Build.objects.all()))
+#     
