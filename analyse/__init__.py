170268ceee3a8c7a4d683b536bc63f987687be57
hukai <hukai@localhost>
2009-12-30 00:25:37 +0800

    changed the backend thread to half hour, changed the page layout.

--- analyse/__init__.py
+++ analyse/__init__.py
@@ -2,5 +2,5 @@ from analyse.background_thread import BackGroundThread
 from analyse.cache import Cache
 from analyse.config import Configs
 
-bgt = BackGroundThread(Cache.INSTANCE().populate, 600)
+bgt = BackGroundThread(Cache.INSTANCE().populate, 3600)
 bgt.launch()
