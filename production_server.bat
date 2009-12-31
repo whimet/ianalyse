cd4e4726a6dfca002e4c41525520c5fadc52d164
hukai <hukai@localhost>
2009-12-31 19:22:46 +0800

    fixed the path issue.

--- production_server.bat
+++ production_server.bat
@@ -1,5 +1,5 @@
 cd `pwd`
 set PYTHONPATH=..;.;$PYTHONPATH
 set DJANGO_SETTINGS_MODULE=ianalyse.settings
-python c:\python26\script\django-admin.py  syncdb
-python c:\python26\script\django-admin.py runserver
+python c:\python26\scripts\django-admin.py  syncdb
+python c:\python26\scripts\django-admin.py runserver
