e58f9db9bf31770948d8d1bb033e9fa966bfeb6c
hukai <hukai@localhost>
2010-01-05 23:19:41 +0800

    for the production env.

--- production_server.bat
+++ production_server.bat
@@ -1,5 +1,6 @@
 cd `pwd`
 set PYTHONPATH=..;.;$PYTHONPATH
 set DJANGO_SETTINGS_MODULE=ianalyse.settings
-python c:\python26\scripts\django-admin.py  syncdb
-python c:\python26\scripts\django-admin.py runserver
+echo please type the non-loopback(not 127.0.0.1 or 0.0.0.0) ip address of your machine:
+set /P ip=
+python c:\python26\scripts\django-admin.py runserver --noreload %ip%:8000
