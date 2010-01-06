411a919da4339cc127d0c3b8d64e86e8daccbf28
hukai <hukai@localhost>
2010-01-06 22:04:34 +0800

    removed the invalid windows script and I hate windows.

--- production_server.bat
+++ production_server.bat
@@ -1,4 +1,4 @@
-cd `pwd`
+@echo off
 set PYTHONPATH=..;.;$PYTHONPATH
 set DJANGO_SETTINGS_MODULE=ianalyse.settings
 echo please type the non-loopback(not 127.0.0.1 or 0.0.0.0) ip address of your machine:
