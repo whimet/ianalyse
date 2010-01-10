1032c6c11c5880500a4db3b17a9a20d74cc510ff
hukai <hukai@localhost>
2010-01-10 22:22:01 +0800

    Finished the README.html

--- production_server.sh
+++ production_server.sh
@@ -1,4 +1,5 @@
-cd `pwd`
 export PYTHONPATH=..:.:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=ianalyse.settings
-django-admin.py runserver --noreload
+echo "please type the non-loopback(not 127.0.0.1 or 0.0.0.0) ip address of your machine:"
+read ip
+django-admin.py runserver --noreload $ip:8000
